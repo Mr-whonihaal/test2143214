d8d1b5ec006433a1705094d8f05327269c8abf22
Mr-whonihaal <78070443+Mr-whonihaal@users.noreply.github.com>
2021-07-27 17:27:23 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+
+var firstname= "Nihaal"
+console.log(firstname)
+
+var namelength=firstname.length
+console.log(namelength)
+
+var nameuppercase= firstname.toUpperCase()
+console.log(nameuppercase)
+
+var age= 13
+console.log(age)
+
+var agestring="10"
+
+var sum=agestring+age
+console.log(sum)
+
+var age1=parseInt(agestring)
+var sum1=age1+age
+console.log(sum1)
+
+var diff=age1-age
+console.log(diff)
+
+var div=age1/age
+console.log(div)
+
+var multi=age1*age
+console.log(multi)
+
+var div= age/age1
+console.log(div)
+
+var something=age%age1 
+console.log(something)
+
+ var div1='age'/'age1'
+ console.log(div1)
+
+ var div=age/0
+ console.log(div)
+
+ var bool=true 
+ console.log(bool)
+
+ var comp=2===3
+ console.log(comp)
+
+ var tree
+ console.log(tree)
+
+ if (3>2){
+
+  console.log("happy")
+ }
+ else{
+  console.log("sad")
+ }
+
+ switch(firstname){
+   case "NIHAAL":
+     console.log("name is in upper case")
+     break
+     case "nihaal":
+       console.log("name is in lower case")
+       break
+       case "Nihaal":
+         console.log("name is in sentence case")
+       }
+       
+
+for(var i=1; i<=5; i++){
+console.log(i)
+  
+}  
+
+for(var i=1; i<=2; i++){
+  console.log('2*'+i+"="+2*i)
+    
+  } 
+
+  var i=0
+  while(i<=5){
+    console.log(i)
+    i=i+1
+  }
+ 
+ 
+ 
